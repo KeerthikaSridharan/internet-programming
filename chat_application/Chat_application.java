c17ec8cecbfbef001ad9676371476a04930e3563
KeerthikaSridharan <99948626+KeerthikaSridharan@users.noreply.github.com>
2023-08-13 07:01:36 +0530

    Add files via upload

---
+++ chat_application/Chat_application.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat_application;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Chat_application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
